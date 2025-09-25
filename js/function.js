66ab5a933b461b04de586e993d01afa2714c239c
Angelina <angelinauvarova943@gmail.com>
2025-09-25 21:28:50 +0500

    добавлены функции проверки длины строки, палиндрома и извлечения цифр

--- js/function.js
+++ js/function.js
@@ -12,18 +12,3 @@ function extractNumber(input) {
   const digits = str.replace(/\D/g, '');
   return digits === '' ? NaN : parseInt(digits, 10);
 }
-
-console.log(checkStringLength('проверяемая строка', 20));
-console.log(checkStringLength('проверяемая строка', 18));
-console.log(checkStringLength('проверяемая строка', 10));
-
-console.log(isPalindrome('топот'));
-console.log(isPalindrome('ДовОд'));
-console.log(isPalindrome('Кекс'));
-console.log(isPalindrome('Лёша на полке клопа нашёл '));
-
-console.log(extractNumber('2023 год'));
-console.log(extractNumber('ECMAScript 2022'));
-console.log(extractNumber('1 кефир, 0.5 батона'));
-console.log(extractNumber('агент 007'));
-console.log(extractNumber('а я томат'));
